94cf46c292dbf73e977261e83fed32b1c4b447e2
juliabandeira <juveigas@gmail.com>
2023-04-12 15:28:54 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from "./Layout";
+import Home from "./Home";
 import "./App.css";
+import CurrencyConverter from "./CurrencyConverter";
 
 const App = () => {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route path="/" exact render={() => <h1>Hello world</h1>} />
+          <Route path="/" exact component={Home} />
+          <Route path="/currencyconverter" component={CurrencyConverter} />
           <Route render={() => <h1>404 Not found</h1>} />
         </Switch>
       </Layout>
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,7 @@
-// Home.js
 import React from "react";
 import currencies from "./utils/currencies";
+import { checkStatus, json } from "./utils/fetchUtils";
+import CurrencyTable from "./CurrencyTable";
 
 class Home extends React.Component {
   constructor() {
@@ -8,15 +9,43 @@ class Home extends React.Component {
     this.state = {
       base: "USD",
       rates: null,
+      loading: true,
     };
+  }
+
+  componentDidMount() {
+    this.getRatesData(this.state.base);
   }
 
   changeBase = (event) => {
     this.setState({ base: event.target.value });
+    this.getRatesData(event.target.value);
+  };
+
+  getRatesData = (base) => {
+    this.setState({ loading: true });
+    fetch(`https://api.frankfurter.app/latest?from=${base}`)
+      .then(checkStatus)
+      .then(json)
+      .then((data) => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        const rates = Object.keys(data.rates)
+          .filter((acronym) => acronym !== base)
+          .map((acronym) => ({
+            acronym,
+            rate: data.rates[acronym],
+            name: currencies[acronym].name,
+            symbol: currencies[acronym].symbol,
+          }));
+        this.setState({ rates, loading: false });
+      })
+      .catch((error) => console.error(error.message));
   };
 
   render() {
-    const { base, rates } = this.state;
+    const { base, rates, loading } = this.state;
 
     return (
       <React.Fragment>
@@ -28,6 +57,7 @@ class Home extends React.Component {
             value={base}
             onChange={this.changeBase}
             className="form-control form-control-lg mb-2"
+            disabled={loading}
           >
             {Object.keys(currencies).map((currencyAcronym) => (
               <option key={currencyAcronym} value={currencyAcronym}>
@@ -36,6 +66,7 @@ class Home extends React.Component {
             ))}
           </select>
         </form>
+        <CurrencyTable base={base} rates={rates} />
       </React.Fragment>
     );
   }
