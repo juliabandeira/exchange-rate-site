6896c2f41f526126eca82a00570e1987206dccfa
juliabandeira <juveigas@gmail.com>
2023-04-13 11:02:04 +0100

    03

--- src/CurrencyConverter.js
+++ src/CurrencyConverter.js
@@ -1,23 +1,30 @@
 import React from "react";
 import currencies from "./utils/currencies";
+import Chart from "chart.js/auto";
 import { checkStatus, json } from "./utils/fetchUtils";
 
 class CurrencyConverter extends React.Component {
   constructor(props) {
     super(props);
+    const params = new URLSearchParams(props.location.search);
+    console.log(params.get("base"), params.get("quote"));
+
     this.state = {
-      rate: 109.55,
-      baseAcronym: "USD",
-      baseValue: 1,
-      quoteAcronym: "JPY",
-      quoteValue: 1 * 109.55,
+      rate: 0,
+      baseAcronym: params.get("base") || "USD",
+      baseValue: 0,
+      quoteAcronym: params.get("quote") || "JPY",
+      quoteValue: 0,
       loading: false,
     };
+
+    this.chartRef = React.createRef();
   }
 
   componentDidMount() {
     const { baseAcronym, quoteAcronym } = this.state;
     this.getRate(baseAcronym, quoteAcronym);
+    this.getHistoricalRates(baseAcronym, quoteAcronym);
   }
 
   getRate = (base, quote) => {
@@ -60,6 +67,7 @@ class CurrencyConverter extends React.Component {
     const baseAcronym = event.target.value;
     this.setState({ baseAcronym });
     this.getRate(baseAcronym, this.state.quoteAcronym);
+    this.getHistoricalRates(baseAcronym, this.state.quoteAcronym);
   };
 
   changeBaseValue = (event) => {
@@ -78,6 +86,7 @@ class CurrencyConverter extends React.Component {
     const quoteAcronym = event.target.value;
     this.setState({ quoteAcronym });
     this.getRate(this.state.baseAcronym, quoteAcronym);
+    this.getHistoricalRates(this.state.baseAcronym, quoteAcronym);
   };
 
   changeQuoteValue = (event) => {
@@ -89,6 +98,52 @@ class CurrencyConverter extends React.Component {
     this.setState({
       quoteValue: event.target.value,
       baseValue,
+    });
+  };
+
+  getHistoricalRates = (base, quote) => {
+    const endDate = new Date().toISOString().split("T")[0];
+    const startDate = new Date(new Date().getTime() - 30 * 24 * 60 * 60 * 1000)
+      .toISOString()
+      .split("T")[0];
+    fetch(
+      `https://api.frankfurter.app/${startDate}..${endDate}?from=${base}&to=${quote}`
+    )
+      .then(checkStatus)
+      .then(json)
+      .then((data) => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        const chartLabels = Object.keys(data.rates);
+        const chartData = Object.values(data.rates).map((rate) => rate[quote]);
+        const chartLabel = `${base}/${quote}`;
+        this.buildChart(chartLabels, chartData, chartLabel);
+      })
+      .catch((error) => console.error(error.message));
+  };
+
+  buildChart = (labels, data, label) => {
+    const chartRef = this.chartRef.current.getContext("2d");
+    if (typeof this.chart !== "undefined") {
+      this.chart.destroy();
+    }
+    this.chart = new Chart(this.chartRef.current.getContext("2d"), {
+      type: "line",
+      data: {
+        labels,
+        datasets: [
+          {
+            label: label,
+            data,
+            fill: false,
+            tension: 0,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+      },
     });
   };
 
@@ -169,6 +224,7 @@ class CurrencyConverter extends React.Component {
             </small>
           </div>
         </form>
+        <canvas ref={this.chartRef} />
       </React.Fragment>
     );
   }
--- src/CurrencyTable.js
+++ src/CurrencyTable.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CurrencyTable = (props) => {
   const { base, rates } = props;
@@ -19,8 +20,12 @@ const CurrencyTable = (props) => {
       <tbody>
         {rates.map((currency) => (
           <tr key={currency.acronym}>
-            <td className="pl-4 py-2">
-              {currency.name} <small>({currency.acronym})</small>
+            <td className="text-right pr-4 py-2">
+              <Link
+                to={`/currencyconverter?base=${base}&quote=${currency.acronym}`}
+              >
+                {currency.rate.toFixed(6)}
+              </Link>
             </td>
             <td className="text-right pr-4 py-2">{currency.rate.toFixed(6)}</td>
           </tr>
